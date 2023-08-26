b0b94db620de459d02fda51d6ca683262717c592
Sandun Rathsara <sandun@surge.global>
2023-08-27 00:21:42 +0530

    removed default next configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/pwa-512x512.png.png"></link>
         <meta name="theme-color" content="#000" />
+        <title>Digis Support Portal</title>
+        <meta name="description" content="Installable web support request portal for" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
