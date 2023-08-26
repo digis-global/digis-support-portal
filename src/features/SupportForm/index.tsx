5728abe3bebf35fa9d554a818a7d967ba5a35134
Sandun Rathsara <sandun@surge.global>
2023-08-27 01:47:37 +0530

    made the page responsive

--- src/features/SupportForm/index.tsx
+++ src/features/SupportForm/index.tsx
@@ -8,35 +8,53 @@ const SupportForm: FC = () => {
     return (
         <>
             {messageContext}
-            <Layout>
-                <Form onFinish={(values: { support: string }) => actions.sendRequest(values.support)} layout="vertical">
-                    <Layout.Header style={{ backgroundColor: "white", padding: '10px' }}>
-                        <div style={styles.headerContainer}>
-                            <Typography.Title><span style={{ color: "#1057A4" }}>Digi</span><span style={{ color: "#FDB400" }}>s</span> <span style={{ color: "#1057A4" }}>Support</span></Typography.Title>
-                        </div>
-                    </Layout.Header>
-                    <Layout.Content style={{ padding: '5px', marginTop: '1rem', backgroundColor: 'white' }}>
-                        <Form.Item label="Explain us your problem" name="support">
-                            <Input.TextArea rows={7} />
-                        </Form.Item>
-                        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                            <Form.Item>
-                                <Button loading={state.loading} type="primary" htmlType="submit">Send</Button>
-                            </Form.Item>
-                        </div>
-                    </Layout.Content>
-                </Form>
+            <Layout style={{ backgroundColor: 'white' }}>
+                <div style={state.isBigScreen ? styles.formContainer : {}}>
+                    <div style={state.isBigScreen ? styles.responsiveContainer : {}}>
+                        <Form onFinish={(values: { support: string }) => actions.sendRequest(values.support)} layout="vertical">
+                            <Layout.Header style={styles.header}>
+                                <div style={styles.headerContainer}>
+                                    <Typography.Title><span style={styles.blueColor}>Digi</span><span style={styles.goldColor}>s</span> <span style={styles.blueColor}>Support</span></Typography.Title>
+                                </div>
+                            </Layout.Header>
+                            <Layout.Content style={styles.contentContainer}>
+                                <Form.Item label="Explain us your problem" name="support">
+                                    <Input.TextArea rows={7} />
+                                </Form.Item>
+                                <div style={styles.buttonContainer}>
+                                    <Form.Item>
+                                        <Button loading={state.loading} type="primary" htmlType="submit">Send</Button>
+                                    </Form.Item>
+                                </div>
+                            </Layout.Content>
+                        </Form>
+                    </div>
+                </div>
             </Layout>
         </>
     )
 }
 
 type TStyles = {
+    formContainer: CSSProperties
+    responsiveContainer: CSSProperties
+    header: CSSProperties
     headerContainer: CSSProperties
+    contentContainer: CSSProperties
+    buttonContainer: CSSProperties
+    blueColor: CSSProperties,
+    goldColor: CSSProperties,
 }
 
 const styles: TStyles = {
-    headerContainer: { display: 'flex', justifyContent: 'center', alignItems: 'center' }
+    formContainer: { display: 'flex', justifyContent: 'center' },
+    responsiveContainer: { width: '50vw' },
+    header: { backgroundColor: "white", padding: '10px' },
+    headerContainer: { display: 'flex', justifyContent: 'center', alignItems: 'center' },
+    contentContainer: { padding: '5px', marginTop: '1rem', },
+    buttonContainer: { display: 'flex', justifyContent: 'flex-end' },
+    blueColor: { color: "#1057A4" },
+    goldColor: { color: "#FDB400" }
 }
 
 export default SupportForm
