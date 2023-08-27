70501f98c8153f967e784727c63e676eb449d6cc
Sandun Rathsara <sandun@surge.global>
2023-08-27 09:27:21 +0530

    font size increased

--- src/features/SupportForm/index.tsx
+++ src/features/SupportForm/index.tsx
@@ -9,7 +9,7 @@ const SupportForm: FC = () => {
     return (
         <>
             {messageContext}
-            <Layout style={{ backgroundColor: 'white' }}>
+            <Layout style={styles.layout}>
                 <div style={state.isBigScreen ? styles.formContainer : {}}>
                     <div style={state.isBigScreen ? styles.responsiveContainer : {}}>
                         <Form ref={state.form} onFinish={(values: { support: string }) => actions.sendRequest(values.support)} layout="vertical">
@@ -19,12 +19,12 @@ const SupportForm: FC = () => {
                                 </div>
                             </Layout.Header>
                             <Layout.Content style={styles.contentContainer}>
-                                <Form.Item label="Explain us your problem" name="support">
+                                <Form.Item label={<Typography.Title level={4} >Explain us your problem</Typography.Title>} name="support">
                                     <Input.TextArea rows={7} />
                                 </Form.Item>
                                 <div style={styles.buttonContainer}>
-                                    <Form.Item style={{ flex: 1 }}>
-                                        <Button loading={state.loading} style={{ width: '100%', height: '3rem' }} type="primary" htmlType="submit" icon={<SendOutlined />}>Send</Button>
+                                    <Form.Item style={styles.buttonItem}>
+                                        <Button loading={state.loading} style={styles.submitButton} type="primary" htmlType="submit" icon={<SendOutlined />}>Send</Button>
                                     </Form.Item>
                                 </div>
                             </Layout.Content>
@@ -37,6 +37,7 @@ const SupportForm: FC = () => {
 }
 
 type TStyles = {
+    layout: CSSProperties
     formContainer: CSSProperties
     responsiveContainer: CSSProperties
     header: CSSProperties
@@ -45,9 +46,12 @@ type TStyles = {
     buttonContainer: CSSProperties
     blueColor: CSSProperties,
     goldColor: CSSProperties,
+    buttonItem: CSSProperties,
+    submitButton: CSSProperties,
 }
 
 const styles: TStyles = {
+    layout: { backgroundColor: "white" },
     formContainer: { display: 'flex', justifyContent: 'center' },
     responsiveContainer: { width: '50vw' },
     header: { backgroundColor: "white", padding: '10px' },
@@ -55,7 +59,9 @@ const styles: TStyles = {
     contentContainer: { padding: '5px', marginTop: '1rem', },
     buttonContainer: { display: 'flex' },
     blueColor: { color: "#1057A4" },
-    goldColor: { color: "#FDB400" }
+    goldColor: { color: "#FDB400" },
+    buttonItem: { flex: 1 },
+    submitButton: { width: '100%', height: '3rem', fontSize: '18px' }
 }
 
 export default SupportForm
